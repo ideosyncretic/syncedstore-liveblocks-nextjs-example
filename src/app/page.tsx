4544fdf6d655534124bd17b1ce03e6ab95f4f0b1
ideosyncretic <sprazzeus@gmail.com>
2024-03-25 19:32:26 +0800

    Add minimal example of SyncedStore with Liveblocks

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,130 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+"use client";
+
+import * as Y from "yjs";
+import { useSyncedStore } from "@syncedstore/react";
+import { faker } from "@faker-js/faker";
+import { RoomProvider, useRoom } from "./liveblocks.config";
+import LiveblocksProvider from "@liveblocks/yjs";
+import { getYjsDoc, syncedStore } from "@syncedstore/core";
+import { useEffect, useState } from "react";
+
+import "./page.module.css";
+
+const yDoc = new Y.Doc();
+const store = syncedStore<{
+  arrayOfStrings: Array<string>;
+  arrayOfObjects: Array<{
+    id: string;
+    label: string;
+  }>;
+  topLevelObject: {
+    nestedArrayOfObjects?: Array<{
+      id: string;
+      label: string;
+    }>;
+  };
+}>(
+  {
+    arrayOfStrings: [],
+    arrayOfObjects: [],
+    topLevelObject: {},
+  },
+  yDoc
+);
+
+function Demo() {
+  const room = useRoom();
+  const [doc, setDoc] = useState<Y.Doc>();
+  const [provider, setProvider] = useState<unknown>();
+
+  useEffect(() => {
+    const yDoc = getYjsDoc(store);
+    const yProvider = new LiveblocksProvider(room, yDoc);
+
+    setDoc(yDoc);
+    setProvider(yProvider);
+
+    return () => {
+      yDoc?.destroy();
+      yProvider?.destroy();
+    };
+  }, [room]);
+
+  const state = useSyncedStore(store);
+
+  if (!doc || !provider) {
+    return null;
+  }
+
+  return (
+    <div className="Demo">
+      <h1>
+        Liveblocks shows objects in arrays as `[object Object]` when Yjs and
+        SyncedStore are correct.
+      </h1>
+      {/* <h1>
+        When updating contents of a nested array of objects (in top-level Y.Map)
+        SyncedStore updates store/Y.Doc correctly, but does not trigger React to
+        re-render
+      </h1> */}
+      <h3>Stringified JSON from SyncedStore (synced to Liveblocks room):</h3>
+      <pre>{JSON.stringify(state, null, 2)}</pre>
+      <div>
+        {/* <button onClick={() => console.log(getYjsDoc(store)?.toJSON())}>
+          Console log Yjs doc as JSON
+        </button>
+        <button
+          onClick={() => {
+            state.arrayOfStrings.push(`${faker.word.words()}`);
+          }}
+        >
+          Add string to top level array
+        </button> */}
+        <button
+          onClick={() => {
+            state.arrayOfObjects.push({
+              id: `${faker.string.uuid()}`,
+              label: `${faker.word.words()}`,
+            });
+          }}
+        >
+          Add object to top level array
+        </button>
+        <button
+          onClick={() => {
+            if (!state.topLevelObject.nestedArrayOfObjects) {
+              state.topLevelObject.nestedArrayOfObjects = [];
+            }
+            state.topLevelObject.nestedArrayOfObjects?.push({
+              id: `${faker.string.uuid()}`,
+              label: `${faker.word.words()}`,
+            });
+          }}
+        >
+          Add object to nested array
+        </button>
+        <button
+          onClick={() => {
+            // Top level arrays are read-only, cannot be reassigned
+            state.arrayOfStrings.splice(0, state.arrayOfStrings.length);
+            state.arrayOfObjects.splice(0, state.arrayOfObjects.length);
+            state.topLevelObject.nestedArrayOfObjects?.splice(
+              0,
+              state.topLevelObject.nestedArrayOfObjects.length
+            );
+          }}
+        >
+          Clear data
+        </button>
+      </div>
+    </div>
+  );
+}
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    <RoomProvider id="codesandbox-liveblocks" initialPresence={{}}>
+      <Demo />
+    </RoomProvider>
   );
 }
